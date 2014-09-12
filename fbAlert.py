ddbc49f7a93efd8910b14e4bed108e4c8b24bba9
sjoenH <henryskorpe@gmail.com>
2014-09-12 14:30:01 +0200

    Added some comments. Added reading of local file

--- fbAlert.py
+++ fbAlert.py
@@ -1,13 +1,17 @@
 import time
-import datetime
+import datetime #Debugging: Will use this later for printing out date.
+
+import os
 
 import RPi.GPIO as GPIO
 
 GPIO.setup(7,GPIO.OUT)
 
+filefoo = open('filefoo.txt','r+') #Checking last remembered nr. of likes.
+
 def blink():
 	GPIO.output(7,True)
-	time.sleep(1)
+	time.sleep(1) #Timedelay
 	GPIO.output(7,False)
 
 while True:
