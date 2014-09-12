e28634b98eab2629448cd163191ac009d6d3f096
SjoenH <henryskorpe@gmail.com>
2014-09-12 14:14:32 +0200

    starting some debugging
    Led wouldn’t change.

--- fbAlert.py
+++ fbAlert.py
@@ -5,12 +5,10 @@ import RPi.GPIO as GPIO
 
 GPIO.setup(7,GPIO.OUT)
 
-def main():
-	while True:
-		GPIO.output(7,True)
-		time.sleep(1)
-		GPIO.output(7,False)
-		pass
-	pass
+def blink():
+	GPIO.output(7,True)
+	time.sleep(1)
+	GPIO.output(7,False)
 
-main()
+
+blink()
