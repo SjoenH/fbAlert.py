4f64d6c3235617fac16e69a6ed893dbddde421f2
sjoenH <henryskorpe@gmail.com>
2014-09-12 15:30:03 +0200

    commented code

--- fbAlert.py
+++ fbAlert.py
@@ -12,10 +12,10 @@ filefoo = open('filefoo.txt','r+') #Checking last remembered nr. of likes.
 
 def blink():
 	GPIO.output(7,True)
-	time.sleep(1) #Timedelay
+	time.sleep(1) #Timedelay. Set to the amount of time you want the LED to shine.
 	GPIO.output(7,False)
 
 while True:
 	blink()
-	time.sleep(3)
+	time.sleep(2)
 	pass
