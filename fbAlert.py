e03647115ef261cb2b2b5394b1cfa244a5c8fcc0
sjoenH <henryskorpe@gmail.com>
2014-09-13 04:20:05 +0200

    Only prints out currentLikes when it is a change.

--- fbAlert.py
+++ fbAlert.py
@@ -9,26 +9,26 @@ GPIO.setup(7,GPIO.OUT)
 filefoo = open('filefoo.txt','r+') #Checking last remembered nr. of likes.
 likes = filefoo.read()
 
-def blink():
+def blink(a):
+	print(currentLikes)
 	GPIO.output(7,True)
-	time.sleep(1) #Timedelay. Set to the amount of time you want the LED to shine.
+	time.sleep(a) #Timedelay. Set to the amount of time you want the LED to shine.
 	GPIO.output(7,False)
 
 while True:
 	r = requests.get('https://graph.facebook.com/695493627155983')
 	j = r.json()
-	currentLikes = j['likes']
+	currentLikes = j['likes'] #Seperates(?) the likes section from the rest of the JSON object.
 	writeLikes = str(currentLikes)
 	if likes < currentLikes :
 		likes = currentLikes
 		filefoo.write(writeLikes)
 		print("Someone Liked Your Site!")
-		blink()
+		blink(2)
 	elif likes > currentLikes :
 		print("It's a sad day... :(")
 		likes = currentLikes
 		filefoo.write(writeLikes)
-		blink()
+		blink(1)
 	else :
-		print(currentLikes)
-		time.sleep(1)
+		time.sleep(1) #How often you want to check FB page.
