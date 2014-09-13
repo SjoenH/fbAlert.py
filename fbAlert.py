6855c0ec8eae0aa40ab6f031917dddac8e5cda2a
sjoenH <henryskorpe@gmail.com>
2014-09-13 04:13:59 +0200

    Removed graph api
    Now uses HTTP Request to get a JSON object of the page. Reads only the
    “Like” section and saves it as an variable.
    #No Need for an Access Key! :D

--- fbAlert.py
+++ fbAlert.py
@@ -1,14 +1,13 @@
 import time
 import datetime #Debugging: Will use this later for printing out date.
 
-import facebook #Graph api. Might want to install manually.
-import os
+import requests
 
 import RPi.GPIO as GPIO
-
 GPIO.setup(7,GPIO.OUT)
 
 filefoo = open('filefoo.txt','r+') #Checking last remembered nr. of likes.
+likes = filefoo.read()
 
 def blink():
 	GPIO.output(7,True)
@@ -16,6 +15,20 @@ def blink():
 	GPIO.output(7,False)
 
 while True:
-	blink()
-	time.sleep(2)
-	pass
+	r = requests.get('https://graph.facebook.com/695493627155983')
+	j = r.json()
+	currentLikes = j['likes']
+	writeLikes = str(currentLikes)
+	if likes < currentLikes :
+		likes = currentLikes
+		filefoo.write(writeLikes)
+		print("Someone Liked Your Site!")
+		blink()
+	elif likes > currentLikes :
+		print("It's a sad day... :(")
+		likes = currentLikes
+		filefoo.write(writeLikes)
+		blink()
+	else :
+		print(currentLikes)
+		time.sleep(1)
