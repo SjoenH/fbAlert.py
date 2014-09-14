7d5bac2b4db7a9f5eb6f0f5d5c37236ebb3488bb
sjoenH <henryskorpe@gmail.com>
2014-09-14 22:45:23 +0200

    trying to fix issue #1 and #2

--- fbAlert.py
+++ fbAlert.py
@@ -16,23 +16,27 @@ def blink(a):
 	time.sleep(a) #Timedelay. Set to the amount of time you want the LED to shine.
 	GPIO.output(7,False)
 
+
 loop = 'forever'
 while loop == 'forever':
-	r = requests.get('https://graph.facebook.com/695493627155983') #Can change with whatever FB-page.
-	j = r.json()
-	currentLikes = j['likes'] #Seperates(?) the likes section from the rest of the JSON object.
-	writeLikes = str(currentLikes)
-	if likes < currentLikes :
-		likes = currentLikes
-		# filefoo.truncate(0)
-		# filefoo.write(writeLikes)
-		print("Someone Liked Your Site!")
-		blink(2)
-	elif likes > currentLikes :
-		print("It's a sad day... :(")
-		likes = currentLikes
-		# filefoo.truncate(0)
-		# filefoo.write(writeLikes)
-		blink(1)
+	if requests.exeptions.ConnectionError():
+			time.sleep(60)
 	else :
-		time.sleep(1) #How often you want to check FB page.
+		r = requests.get('https://graph.facebook.com/695493627155983') #Can change with whatever FB-page.
+		j = r.json()
+		currentLikes = j['likes'] #Seperates(?) the likes section from the rest of the JSON object.
+		writeLikes = str(currentLikes)
+		if likes < currentLikes :
+			likes = currentLikes
+			# filefoo.truncate(0)
+			# filefoo.write(writeLikes)
+			print("Someone Liked Your Site!")
+			blink(2)
+		elif likes > currentLikes :
+			print("It's a sad day... :(")
+			likes = currentLikes
+			# filefoo.truncate(0)
+			# filefoo.write(writeLikes)
+			blink(1)
+		else :
+			time.sleep(1) #How often you want to check FB page.
