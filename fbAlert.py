a5d70edf4c126ec9890852280a0baf7a9504972a
sjoenH <henryskorpe@gmail.com>
2014-09-14 22:52:31 +0200

    Last didn't work. Trying except exceptions

--- fbAlert.py
+++ fbAlert.py
@@ -16,27 +16,26 @@ def blink(a):
 	time.sleep(a) #Timedelay. Set to the amount of time you want the LED to shine.
 	GPIO.output(7,False)
 
-
+except Exception:
+	pass
+	
 loop = 'forever'
 while loop == 'forever':
-	if requests.get('https://graph.facebook.com/695493627155983') == requests.exceptions.ConnectionError():
-			time.sleep(60)
+	r = requests.get('https://graph.facebook.com/695493627155983') #Can change with whatever FB-page.
+	j = r.json()
+	currentLikes = j['likes'] #Seperates(?) the likes section from the rest of the JSON object.
+	writeLikes = str(currentLikes)
+	if likes < currentLikes :
+		likes = currentLikes
+		# filefoo.truncate(0)
+		# filefoo.write(writeLikes)
+		print("Someone Liked Your Site!")
+		blink(2)
+	elif likes > currentLikes :
+		print("It's a sad day... :(")
+		likes = currentLikes
+		# filefoo.truncate(0)
+		# filefoo.write(writeLikes)
+		blink(1)
 	else :
-		r = requests.get('https://graph.facebook.com/695493627155983') #Can change with whatever FB-page.
-		j = r.json()
-		currentLikes = j['likes'] #Seperates(?) the likes section from the rest of the JSON object.
-		writeLikes = str(currentLikes)
-		if likes < currentLikes :
-			likes = currentLikes
-			# filefoo.truncate(0)
-			# filefoo.write(writeLikes)
-			print("Someone Liked Your Site!")
-			blink(2)
-		elif likes > currentLikes :
-			print("It's a sad day... :(")
-			likes = currentLikes
-			# filefoo.truncate(0)
-			# filefoo.write(writeLikes)
-			blink(1)
-		else :
-			time.sleep(1) #How often you want to check FB page.
+		time.sleep(1) #How often you want to check FB page.
