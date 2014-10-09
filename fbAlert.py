417491637d8ba08fe85ea732c07729685c3aa1e9
sjoenH <henryskorpe@gmail.com>
2014-10-09 23:48:02 +0200

    edit.
    should work.

--- fbAlert.py
+++ fbAlert.py
@@ -3,8 +3,8 @@ import datetime #Debugging: Will use this later for printing out date.
 
 import requests
 
-import RPi.GPIO as GPIO
-GPIO.setup(7,GPIO.OUT)
+# import RPi.GPIO as GPIO
+# GPIO.setup(7,GPIO.OUT)
 
 def readLikes():
 	f = open('likes', 'r+')
@@ -20,17 +20,16 @@ def writeLikes(a):
 
 def blink(a):
 	print(currentLikes)
-	GPIO.output(7,True)
-	time.sleep(a) #Timedelay. Set to the amount of time you want the LED to shine.
-	GPIO.output(7,False)
+	# GPIO.output(7,True)
+	# time.sleep(a) #Timedelay. Set to the amount of time you want the LED to shine.
+	# GPIO.output(7,False)
 	pass
 
 while True :
 	time.sleep(10) #Waiting before trying to request site.
 	r = requests.get('https://graph.facebook.com/695493627155983') #Can change with whatever FB-page.
 	if r.status_code == requests.codes.ok : #Checking if website returns a 'OK value'.
-		j = r.json()
-		currentLikes = j['likes'] #Seperates(?) the likes section from the rest of the JSON object.
+		currentLikes = r.json()['likes'] #Seperates(?) the likes section from the rest of the JSON object.
 		if likes < currentLikes :
 			likes = currentLikes
 			writeLikes(likes)
